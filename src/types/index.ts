409368f324e0177ef228156df11d15682d5dae1f
Nelson Wei <nelson@ensemble.com>
2025-01-21 14:34:24 -0800

    test

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,17 @@
 /**
- *
+ * {
+ *   "webPath": "/blogs/aspernatur-molestias-caste",
+ *   "resourcePath": "/blogs/aspernatur-molestias-caste.md",
+ *   "name": "Fresh Chillies Salad",
+ *   "lastModified": "1731143207086",
+ *   "title": "White Wine And Turkey Pie",
+ *   "image": "https://loremflickr.com/1147/2155?lock=8202902739233803",
+ *   "description": "Three almonds with spinach, onion, snowpea sprouts, kale and bacon. With a side of baked blackberry, and your choice of camellia tea oil or bok choy.",
+ *   "category": "Bulgarian",
+ *   "author": "W.G. Sebald",
+ *   "date": "1714224070019",
+ *   "objectID": "fae28cfcddd8c25206ce85b67fc7bc20"
+ * }
  */
 export interface AlgoliaRecord extends Record<string, unknown> {
   webPath?: string;
