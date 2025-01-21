0f0801d6caf5f8c1a09ec2f4ed76abd9bf455fe7
Nelson Wei <nelson@ensemble.com>
2025-01-20 18:04:49 -0800

    Added tests

---
+++ src/utils/__tests__/stringUtils.spec.ts
@@ -0,0 +1,12 @@
+import md5 from '../stringUtils';
+
+describe('stringUtils functions', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  // Assert if setTimeout was called properly
+  it('generates md5 hash accordingly', async () => {
+    expect(md5('asdf')).toBe('912ec803b2ce49e4a541068d495ab570');
+  });
+});
