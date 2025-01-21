5c4c4f9a8440aa0a3c978f552678f1d59e837e0f
Nelson Wei <nelson@ensemble.com>
2025-01-21 14:45:32 -0800

    Customize log

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,5 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/', '/dist/', , '/lib/', 'constants.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
